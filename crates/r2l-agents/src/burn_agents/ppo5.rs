18a125cb8fc7d4ebe21b6eb813e7ba8c7de94f70
Gabor Feher <decetkriozmus@gmail.com>
2026-03-22 10:16:56 +0900

    started working on new burn ppo

---
+++ crates/r2l-agents/src/burn_agents/ppo5.rs
@@ -0,0 +1,62 @@
+use crate::Policy;
+use burn::{
+    module::{AutodiffModule, ModuleDisplay},
+    tensor::{Tensor as BurnTensor, backend::AutodiffBackend},
+};
+use r2l_burn_lm::learning_module::ParalellActorCriticLM;
+use r2l_core::{agents::Agent5, sampler5::buffer::TrajectoryContainer};
+use r2l_core::{agents::TensorOfAgent, policies::LearningModule};
+
+// impl<B: AutodiffBackend, D: AutodiffModule<B> + ModuleDisplay + Policy<Tensor = BurnTensor<B, 1>>>
+//     BurnPPPHooksTrait<B, D> for EmptyBurnPPOHooks
+// where
+//     <D as AutodiffModule<B>>::InnerModule: ModuleDisplay,
+// {
+// }
+
+pub struct BurnPPOCore<
+    B: AutodiffBackend,
+    D: AutodiffModule<B> + ModuleDisplay + Policy<Tensor = BurnTensor<B, 1>>,
+> where
+    <D as AutodiffModule<B>>::InnerModule: ModuleDisplay,
+{
+    pub lm: ParalellActorCriticLM<B, D>,
+    pub clip_range: f32,
+    pub sample_size: usize,
+    pub gamma: f32,
+    pub lambda: f32,
+}
+
+pub struct BurnPPO<
+    B: AutodiffBackend,
+    D: AutodiffModule<B> + ModuleDisplay + Policy<Tensor = BurnTensor<B, 1>>,
+> where
+    <D as AutodiffModule<B>>::InnerModule: ModuleDisplay,
+{
+    pub core: BurnPPOCore<B, D>,
+    // pub hooks: Box<dyn BurnPPPHooksTrait<B, D>>,
+}
+
+impl<
+    B: AutodiffBackend<InnerBackend = B>,
+    D: AutodiffModule<B> + ModuleDisplay + Policy<Tensor = BurnTensor<B, 1>>,
+> Agent5 for BurnPPO<B, D>
+where
+    <D as AutodiffModule<B>>::InnerModule:
+        ModuleDisplay + Policy<Tensor = BurnTensor<B::InnerBackend, 1>>,
+{
+    type Tensor = BurnTensor<B, 1>;
+
+    type Policy = D::InnerModule;
+
+    fn policy(&self) -> Self::Policy {
+        todo!()
+    }
+
+    fn learn<C: TrajectoryContainer<Tensor = Self::Tensor>>(
+        &mut self,
+        buffers: &[C],
+    ) -> anyhow::Result<()> {
+        todo!()
+    }
+}
