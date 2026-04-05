2b053f72a74df241f414e64eb03e3779bddc0ab3
Gabor Feher <decetkriozmus@gmail.com>
2026-04-05 10:30:32 +0900

    added new ppo hook trait implemenation for burn

--- crates/r2l-api/src/agents/burn_ppo2.rs
+++ crates/r2l-api/src/agents/burn_ppo2.rs
@@ -1,23 +1,25 @@
-use std::marker::PhantomData;
-
-use burn::tensor::{Tensor as BurnTensor, backend::AutodiffBackend};
-use r2l_agents::ppo2::{PPOModule2, RolloutLearningModule};
+use burn::{
+    grad_clipping::GradientClipping,
+    tensor::{Tensor as BurnTensor, backend::AutodiffBackend},
+};
+use r2l_agents::ppo2::{NewPPO, PPOModule2, RolloutLearningModule};
 use r2l_burn_lm::learning_module::{BurnPolicy, ParalellActorCriticLM, PolicyValuesLosses};
 use r2l_core::policies::{LearningModule, ValueFunction};
 
+use crate::hooks::ppo::PPOHook;
+
 // TODO: finish this in some form
-pub struct R2lBurnLearningModule<
-    B: AutodiffBackend,
-    D: BurnPolicy<B>,
-    V: ValueFunction<Tensor = BurnTensor<B, 1>>,
-> {
+pub struct R2lBurnLearningModule<B: AutodiffBackend, D: BurnPolicy<B>> {
     lm: ParalellActorCriticLM<B, D>,
-    _phantom: PhantomData<(B, D, V)>,
 }
 
-impl<B: AutodiffBackend, D: BurnPolicy<B>, V: ValueFunction<Tensor = BurnTensor<B, 1>>>
-    LearningModule for R2lBurnLearningModule<B, D, V>
-{
+impl<B: AutodiffBackend, D: BurnPolicy<B>> R2lBurnLearningModule<B, D> {
+    pub fn set_grad_clipping(&mut self, gradient_clipping: GradientClipping) {
+        self.lm.set_grad_clipping(gradient_clipping);
+    }
+}
+
+impl<B: AutodiffBackend, D: BurnPolicy<B>> LearningModule for R2lBurnLearningModule<B, D> {
     type Losses = PolicyValuesLosses<B>;
 
     fn update(&mut self, losses: Self::Losses) -> anyhow::Result<()> {
@@ -25,9 +27,7 @@ impl<B: AutodiffBackend, D: BurnPolicy<B>, V: ValueFunction<Tensor = BurnTensor<
     }
 }
 
-impl<B: AutodiffBackend, D: BurnPolicy<B>, V: ValueFunction<Tensor = BurnTensor<B, 1>>>
-    ValueFunction for R2lBurnLearningModule<B, D, V>
-{
+impl<B: AutodiffBackend, D: BurnPolicy<B>> ValueFunction for R2lBurnLearningModule<B, D> {
     type Tensor = BurnTensor<B, 1>;
 
     fn calculate_values(&self, observations: &[Self::Tensor]) -> anyhow::Result<Self::Tensor> {
@@ -35,9 +35,7 @@ impl<B: AutodiffBackend, D: BurnPolicy<B>, V: ValueFunction<Tensor = BurnTensor<
     }
 }
 
-impl<B: AutodiffBackend, D: BurnPolicy<B>, V: ValueFunction<Tensor = BurnTensor<B, 1>>>
-    RolloutLearningModule for R2lBurnLearningModule<B, D, V>
-{
+impl<B: AutodiffBackend, D: BurnPolicy<B>> RolloutLearningModule for R2lBurnLearningModule<B, D> {
     type LearningTensor = BurnTensor<B, 1>;
     type InferenceTensor = BurnTensor<B::InnerBackend, 1>;
     type Policy = D;
@@ -60,7 +58,8 @@ impl<B: AutodiffBackend, D: BurnPolicy<B>, V: ValueFunction<Tensor = BurnTensor<
     }
 }
 
-impl<B: AutodiffBackend, D: BurnPolicy<B>, V: ValueFunction<Tensor = BurnTensor<B, 1>>> PPOModule2
-    for R2lBurnLearningModule<B, D, V>
-{
-}
+impl<B: AutodiffBackend, D: BurnPolicy<B>> PPOModule2 for R2lBurnLearningModule<B, D> {}
+
+pub struct BurnPPO<B: AutodiffBackend, D: BurnPolicy<B>>(
+    pub NewPPO<R2lBurnLearningModule<B, D>, PPOHook<R2lBurnLearningModule<B, D>>>,
+);
--- crates/r2l-api/src/hooks/new_ppo_trait.rs
+++ crates/r2l-api/src/hooks/new_ppo_trait.rs
@@ -1,14 +1,125 @@
 use crate::{
-    agents::candle_ppo2::R2lCandleLearningModule,
+    agents::{burn_ppo2::R2lBurnLearningModule, candle_ppo2::R2lCandleLearningModule},
     hooks::ppo::{BatchStats, PPOHook, PPOHookReporter},
 };
+use burn::{grad_clipping::GradientClipping, tensor::backend::AutodiffBackend};
 use candle_core::Tensor;
 use r2l_agents::{
     HookResult,
     ppo2::{NewPPOBatchData, NewPPOHooksTrait, NewPPOParams, RolloutLearningModule},
 };
+use r2l_burn_lm::learning_module::{BurnPolicy, PolicyValuesLosses as BurnPolicyValuesLosses};
 use r2l_candle_lm::learning_module::PolicyValuesLosses;
 use r2l_core::{distributions::Policy, sampler::buffer::TrajectoryContainer};
+
+impl<B: AutodiffBackend, D: BurnPolicy<B>> NewPPOHooksTrait<R2lBurnLearningModule<B, D>>
+    for PPOHook<R2lBurnLearningModule<B, D>>
+{
+    fn before_learning_hook<
+        T: TrajectoryContainer<Tensor = burn::Tensor<<B as AutodiffBackend>::InnerBackend, 1>>,
+    >(
+        &mut self,
+        _params: &mut NewPPOParams,
+        module: &mut R2lBurnLearningModule<B, D>,
+        _buffers: &[T],
+        advantages: &mut r2l_core::utils::rollout_buffer::Advantages,
+        _returns: &mut r2l_core::utils::rollout_buffer::Returns,
+    ) -> anyhow::Result<HookResult> {
+        self.current_epoch = 0;
+        if self.normalize_advantage {
+            advantages.normalize();
+        }
+        if let Some(max_grad_norm) = self.gradient_clipping {
+            module.set_grad_clipping(GradientClipping::Norm(max_grad_norm));
+        }
+        Ok(HookResult::Continue)
+    }
+
+    fn rollout_hook<
+        T: TrajectoryContainer<Tensor = burn::Tensor<<B as AutodiffBackend>::InnerBackend, 1>>,
+    >(
+        &mut self,
+        _params: &mut NewPPOParams,
+        module: &mut R2lBurnLearningModule<B, D>,
+        buffers: &[T],
+    ) -> anyhow::Result<HookResult> {
+        self.current_epoch += 1;
+        let target_kl_exceeded = if let Some(target_kl) = &mut self.target_kl {
+            target_kl.target_kl_exceeded()
+        } else {
+            false
+        };
+        let should_stop = self.current_epoch == self.total_epochs || target_kl_exceeded;
+        if should_stop {
+            if let Some(PPOHookReporter { report, tx }) = &mut self.reporter {
+                let mut total_rewards: f32 = 0.;
+                let mut total_episodes: usize = 0;
+                for buffer in buffers {
+                    total_rewards += buffer.rewards().sum::<f32>();
+                    total_episodes += buffer.dones().filter(|done| *done).count();
+                }
+                report.avarage_reward = total_rewards / total_episodes as f32;
+                report.std = module.get_policy().std().unwrap();
+                report.learning_rate = 3e-4;
+                tx.send(std::mem::take(report)).unwrap();
+            }
+            Ok(HookResult::Break)
+        } else {
+            Ok(HookResult::Continue)
+        }
+    }
+
+    fn batch_hook(
+        &mut self,
+        params: &mut NewPPOParams,
+        module: &mut R2lBurnLearningModule<B, D>,
+        losses: &mut BurnPolicyValuesLosses<B>,
+        data: &NewPPOBatchData<burn::Tensor<B, 1>>,
+    ) -> anyhow::Result<HookResult> {
+        losses.set_vf_coeff(self.vf_coeff);
+        let entropy = module.get_policy().entropy().unwrap();
+        let entropy_loss = entropy.neg() * self.entropy_coeff;
+        let approx_kl = {
+            let ratio: Vec<f32> = data.ratio.to_data().to_vec().unwrap();
+            let log_ratio: Vec<f32> = data.logp_diff.to_data().to_vec().unwrap();
+            ratio
+                .iter()
+                .zip(log_ratio.iter())
+                .map(|(ratio, log_ratio)| (ratio - 1.) - log_ratio)
+                .sum::<f32>()
+                / ratio.len() as f32
+        };
+
+        if let Some(PPOHookReporter { report, .. }) = &mut self.reporter {
+            let ratio: Vec<f32> = data.ratio.to_data().to_vec().unwrap();
+            let clip_fraction = ratio
+                .iter()
+                .filter(|value| (**value - 1.).abs() > params.clip_range)
+                .count() as f32
+                / ratio.len() as f32;
+            report.collect_batch_data(BatchStats {
+                clip_fraction,
+                policy_loss: losses.policy_loss.to_data().to_vec::<f32>().unwrap()[0],
+                entropy_loss: entropy_loss.to_data().to_vec::<f32>().unwrap()[0],
+                value_loss: losses.value_loss.to_data().to_vec::<f32>().unwrap()[0],
+                approx_kl,
+            });
+        }
+        if self.entropy_coeff != 0. {
+            losses.apply_entropy(entropy_loss);
+        }
+        if let Some(target_kl) = &mut self.target_kl {
+            if approx_kl > 1.5 * target_kl.target {
+                target_kl.target_exceeded = true;
+                Ok(HookResult::Break)
+            } else {
+                Ok(HookResult::Continue)
+            }
+        } else {
+            Ok(HookResult::Continue)
+        }
+    }
+}
 
 impl NewPPOHooksTrait<R2lCandleLearningModule> for PPOHook<R2lCandleLearningModule> {
     fn before_learning_hook<B: TrajectoryContainer<Tensor = candle_core::Tensor>>(
