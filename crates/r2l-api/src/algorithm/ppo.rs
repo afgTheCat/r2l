d8e1b91ccf417a179ae43d475f78a0f0b48f6471
Gabor Feher <decetkriozmus@gmail.com>
2026-04-09 13:23:04 +0000

    moved PPO builder specific things into a central space

--- crates/r2l-api/src/algorithm/ppo.rs
+++ crates/r2l-api/src/algorithm/ppo.rs
@@ -1,2 +1,129 @@
 pub mod burn;
 pub mod candle;
+
+use std::sync::mpsc::Sender;
+
+use r2l_core::{
+    agents::Agent,
+    env_builder::EnvBuilderTrait,
+    on_policy_algorithm::LearningSchedule,
+    sampler::{
+        Location,
+        buffer::TrajectoryBound,
+    },
+};
+
+use crate::{
+    agents::{AgentBuilder, ppo::PPOAgentBuilder},
+    algorithm::OnPolicyAlgorightmBuilder,
+    hooks::ppo::PPOStats,
+};
+
+impl<A, M, EB> OnPolicyAlgorightmBuilder<A, PPOAgentBuilder<M>, EB>
+where
+    A: Agent,
+    EB: EnvBuilderTrait,
+    PPOAgentBuilder<M>: AgentBuilder<Agent = A>,
+{
+    pub fn with_bound<BD2: TrajectoryBound<Tensor = EB::Tensor>>(
+        self,
+        trajectory_bound: BD2,
+    ) -> OnPolicyAlgorightmBuilder<A, PPOAgentBuilder<M>, EB, BD2> {
+        let OnPolicyAlgorightmBuilder {
+            sampler_builder,
+            agent_builder,
+            learning_schedule,
+            ..
+        } = self;
+        OnPolicyAlgorightmBuilder {
+            sampler_builder: sampler_builder.with_bound(trajectory_bound),
+            agent_builder,
+            learning_schedule,
+        }
+    }
+}
+
+impl<A, M, EB, BD> OnPolicyAlgorightmBuilder<A, PPOAgentBuilder<M>, EB, BD>
+where
+    A: Agent,
+    EB: EnvBuilderTrait,
+    BD: TrajectoryBound<Tensor = EB::Tensor>,
+    PPOAgentBuilder<M>: AgentBuilder<Agent = A>,
+{
+    pub fn with_normalize_advantage(mut self, normalize_advantage: bool) -> Self {
+        self.agent_builder.hook_builder = self
+            .agent_builder
+            .hook_builder
+            .with_normalize_advantage(normalize_advantage);
+        self
+    }
+
+    pub fn with_total_epochs(mut self, total_epochs: usize) -> Self {
+        self.agent_builder.hook_builder = self
+            .agent_builder
+            .hook_builder
+            .with_total_epochs(total_epochs);
+        self
+    }
+
+    pub fn with_entropy_coeff(mut self, entropy_coeff: f32) -> Self {
+        self.agent_builder.hook_builder = self
+            .agent_builder
+            .hook_builder
+            .with_entropy_coeff(entropy_coeff);
+        self
+    }
+
+    pub fn with_vf_coeff(mut self, vf_coeff: Option<f32>) -> Self {
+        self.agent_builder.hook_builder = self.agent_builder.hook_builder.with_vf_coeff(vf_coeff);
+        self
+    }
+
+    pub fn with_target_kl(mut self, target_kl: Option<f32>) -> Self {
+        self.agent_builder.hook_builder = self.agent_builder.hook_builder.with_target_kl(target_kl);
+        self
+    }
+
+    pub fn with_gradient_clipping(mut self, gradient_clipping: Option<f32>) -> Self {
+        self.agent_builder.hook_builder = self
+            .agent_builder
+            .hook_builder
+            .with_gradient_clipping(gradient_clipping);
+        self
+    }
+
+    pub fn with_reporter(mut self, tx: Option<Sender<PPOStats>>) -> Self {
+        self.agent_builder.hook_builder = self.agent_builder.hook_builder.with_tx(tx);
+        self
+    }
+
+    pub fn with_clip_range(mut self, clip_range: f32) -> Self {
+        self.agent_builder.ppo_params.clip_range = clip_range;
+        self
+    }
+
+    pub fn with_gamma(mut self, gamma: f32) -> Self {
+        self.agent_builder.ppo_params.gamma = gamma;
+        self
+    }
+
+    pub fn with_lambda(mut self, lambda: f32) -> Self {
+        self.agent_builder.ppo_params.lambda = lambda;
+        self
+    }
+
+    pub fn with_sample_size(mut self, sample_size: usize) -> Self {
+        self.agent_builder.ppo_params.sample_size = sample_size;
+        self
+    }
+
+    pub fn with_location(mut self, location: Location) -> Self {
+        self.sampler_builder = self.sampler_builder.with_location(location);
+        self
+    }
+
+    pub fn with_learning_schedule(mut self, learning_schedule: LearningSchedule) -> Self {
+        self.learning_schedule = learning_schedule;
+        self
+    }
+}
--- crates/r2l-api/src/algorithm/ppo/candle.rs
+++ crates/r2l-api/src/algorithm/ppo/candle.rs
@@ -1,16 +1,13 @@
 use crate::{
     agents::ppo::{CandlePPO, PPOCandleLearningModuleBuilder},
     algorithm::OnPolicyAlgorightmBuilder,
-    hooks::ppo::PPOStats,
     sampler::SamplerBuilder,
 };
-use r2l_core::sampler::Location;
 use r2l_core::{
     env_builder::EnvBuilderTrait,
     on_policy_algorithm::LearningSchedule,
-    sampler::buffer::{StepTrajectoryBound, TrajectoryBound},
+    sampler::buffer::StepTrajectoryBound,
 };
-use std::sync::mpsc::Sender;
 
 pub type PPOCandleAlgorithmBuiler<EB, BD = StepTrajectoryBound<<EB as EnvBuilderTrait>::Tensor>> =
     OnPolicyAlgorightmBuilder<CandlePPO, PPOCandleLearningModuleBuilder, EB, BD>;
@@ -25,103 +22,5 @@ impl<EB: EnvBuilderTrait> PPOCandleAlgorithmBuiler<EB> {
                 current_rollout: 0,
             },
         }
-    }
-}
-
-impl<EB: EnvBuilderTrait, BD: TrajectoryBound<Tensor = EB::Tensor>>
-    PPOCandleAlgorithmBuiler<EB, BD>
-{
-    pub fn with_bound<BD2: TrajectoryBound<Tensor = EB::Tensor>>(
-        self,
-        trajectory_bound: BD2,
-    ) -> PPOCandleAlgorithmBuiler<EB, BD2> {
-        let OnPolicyAlgorightmBuilder {
-            sampler_builder,
-            agent_builder,
-            learning_schedule,
-            ..
-        } = self;
-        OnPolicyAlgorightmBuilder {
-            sampler_builder: sampler_builder.with_bound(trajectory_bound),
-            agent_builder,
-            learning_schedule,
-        }
-    }
-
-    pub fn with_normalize_advantage(mut self, normalize_advantage: bool) -> Self {
-        self.agent_builder.hook_builder = self
-            .agent_builder
-            .hook_builder
-            .with_normalize_advantage(normalize_advantage);
-        self
-    }
-
-    pub fn with_total_epochs(mut self, total_epochs: usize) -> Self {
-        self.agent_builder.hook_builder = self
-            .agent_builder
-            .hook_builder
-            .with_total_epochs(total_epochs);
-        self
-    }
-
-    pub fn with_entropy_coeff(mut self, entropy_coeff: f32) -> Self {
-        self.agent_builder.hook_builder = self
-            .agent_builder
-            .hook_builder
-            .with_entropy_coeff(entropy_coeff);
-        self
-    }
-
-    pub fn with_vf_coeff(mut self, vf_coeff: Option<f32>) -> Self {
-        self.agent_builder.hook_builder = self.agent_builder.hook_builder.with_vf_coeff(vf_coeff);
-        self
-    }
-
-    pub fn with_target_kl(mut self, target_kl: Option<f32>) -> Self {
-        self.agent_builder.hook_builder = self.agent_builder.hook_builder.with_target_kl(target_kl);
-        self
-    }
-
-    pub fn with_gradient_clipping(mut self, gradient_clipping: Option<f32>) -> Self {
-        self.agent_builder.hook_builder = self
-            .agent_builder
-            .hook_builder
-            .with_gradient_clipping(gradient_clipping);
-        self
-    }
-
-    pub fn with_reporter(mut self, tx: Option<Sender<PPOStats>>) -> Self {
-        self.agent_builder.hook_builder = self.agent_builder.hook_builder.with_tx(tx);
-        self
-    }
-
-    pub fn with_clip_range(mut self, clip_range: f32) -> Self {
-        self.agent_builder.ppo_params.clip_range = clip_range;
-        self
-    }
-
-    pub fn with_gamma(mut self, gamma: f32) -> Self {
-        self.agent_builder.ppo_params.gamma = gamma;
-        self
-    }
-
-    pub fn with_lambda(mut self, lambda: f32) -> Self {
-        self.agent_builder.ppo_params.lambda = lambda;
-        self
-    }
-
-    pub fn with_sample_size(mut self, sample_size: usize) -> Self {
-        self.agent_builder.ppo_params.sample_size = sample_size;
-        self
-    }
-
-    pub fn with_location(mut self, location: Location) -> Self {
-        self.sampler_builder = self.sampler_builder.with_location(location);
-        self
-    }
-
-    pub fn with_learning_schedule(mut self, learning_schedule: LearningSchedule) -> Self {
-        self.learning_schedule = learning_schedule;
-        self
     }
 }
