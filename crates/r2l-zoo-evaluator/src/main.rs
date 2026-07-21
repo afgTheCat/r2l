d271f4ba372ab8d4ce52deda2f5159a3004b5458
Gabor Feher <decetkriozmus@gmail.com>
2026-07-21 17:12:28 +0200

    started with the ecaluator

--- crates/r2l-zoo-evaluator/src/main.rs
+++ crates/r2l-zoo-evaluator/src/main.rs
@@ -1,3 +1,9 @@
+// The main reponsibiliy of this code is to:
+// - run extensive test suits if we need be
+//  -- with seeds etc (repro test)
+//  -- with seeds not set
+// - generate statistics, generate figures etc
+
 fn main() {
     println!("Hello, world!");
 }
